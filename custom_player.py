fb16488112b38f61ed5e8b52718c0aaef137b013
Samuel Woldegiorgies <45132231+Samuel-T-W@users.noreply.github.com>
2025-03-23 05:15:39 -0700

    checkpoint

---
+++ custom_player.py
@@ -0,0 +1,141 @@
+from pypokerengine.engine.action_checker import ActionChecker
+from pypokerengine.engine.card import Card
+from pypokerengine.engine.round_manager import RoundManager
+from pypokerengine.players import BasePokerPlayer
+import random as rand
+from pypokerengine.engine.poker_constants import PokerConstants as Const
+import pprint
+
+from pypokerengine.utils.card_utils import estimate_hole_card_win_rate
+
+class CustomPlayer(BasePokerPlayer):
+
+  def declare_action(self, valid_actions, hole_card, round_state):
+    # valid_actions format => [raise_action_pp = pprint.PrettyPrinter(indent=2)
+    # pp = pprint.PrettyPrinter(indent=2)
+    # print("------------ROUND_STATE(RANDOM)--------")
+    # pp.pprint(round_state)
+    # print("------------HOLE_CARD----------")
+    # pp.pprint(hole_card)
+    # print("------------VALID_ACTIONS----------")
+    # pp.pprint(valid_actions)
+    # print("-------------------------------")
+    # print("------------VALID_ACTIONS----------")
+    # estimate_hole_card_win_rate_timed(1000, 2, hole_card, round_state["community_card"])
+    # print("------------VALID_ACTIONS----------")
+    
+    game_state_copy = deepcopy_game_state(round_state)
+    _, action = tree_search(hole_card, round_state, 3, is_maximizing_player=True)
+
+    return action  # action returned here is sent to the poker engine
+
+
+  
+  def receive_game_start_message(self, game_info):
+    pass
+
+  def receive_round_start_message(self, round_count, hole_card, seats):
+    pass
+
+  def receive_street_start_message(self, street, round_state):
+    pass
+
+  def receive_game_update_message(self, action, round_state):
+    pass
+
+  def receive_round_result_message(self, winners, hand_info, round_state):
+    pass
+
+def setup_ai():
+  return CustomPlayer()
+
+
+
+# ------- ------- ------- ------- ------- utils ------- ------- ------- ------- ------- ------- ------- 
+import functools
+import inspect
+import time
+# A decorator to measure the execution time of a function and print its parameters
+def debug_timer(func):
+  @functools.wraps(func)
+  def wrapper(*args, **kwargs):
+      # Get parameter names
+      sig = inspect.signature(func)
+      param_names = list(sig.parameters.keys())
+      
+      # Print function name
+      print(f"Executing function: {func.__name__}")
+      
+      # Print parameters
+      for i, arg in enumerate(args):
+          if i < len(param_names):
+              print(f"  {param_names[i]} = {arg}")
+          else:
+              print(f"  *args[{i}] = {arg}")
+      
+      for key, value in kwargs.items():
+          print(f"  {key} = {value}")
+      
+      # Measure execution time
+      start_time = time.time()
+      result = func(*args, **kwargs)
+      end_time = time.time()
+      
+      # Print execution time
+      print(f"Function {func.__name__} executed in {(end_time - start_time)*1000 :.6f} milliseconds")
+      
+      return result
+  
+  return wrapper
+
+def apply_action(self, game_state, action, bet_amount=0):
+        if game_state["street"] == Const.Street.FINISHED:
+            game_state, events = self._start_next_round(game_state)
+        updated_state, messages = RoundManager.apply_action(game_state, action, bet_amount)
+        return updated_state
+
+def generate_possible_actions(self, game_state):
+        players = game_state["table"].seats.players
+        player_pos = game_state["next_player"]
+        sb_amount = game_state["small_blind_amount"]
+        return ActionChecker.legal_actions(players, player_pos, sb_amount)
+
+@debug_timer
+def estimate_hole_card_win_rate_timed(nb_simulation, nb_player, hole_card, community_card=None):
+    hole_card = [Card.from_str(card) for card in hole_card]
+    if community_card:
+        community_card = [Card.from_str(card) for card in community_card]
+    return estimate_hole_card_win_rate(nb_simulation, nb_player, hole_card, community_card)
+
+def is_last_round(game_state, game_rule):
+        is_round_finished = game_state["street"] == Const.Street.FINISHED
+        is_final_round = game_state["round_count"] == game_rule["max_round"]
+        is_winner_decided = len([1 for p in game_state["table"].seats.players if p.stack!=0])==1
+        return is_round_finished and (is_final_round or is_winner_decided)
+
+
+@debug_timer
+def tree_search(hole_card, round_state, depth, is_maximizing_player=True):
+  if is_last_round(round_state, round_state["game_rule"]):
+    sign = -1 if is_maximizing_player else 1 # reverse since the player is before is the one to choose
+    winners, _, _ = GameEvaluator.judge(round_state["table"])
+    
+  # Base case for recursion
+  if depth == 0 or round_state["street"] == Const.Street.FINISHED:
+    return estimate_hole_card_win_rate_timed(30, 2, hole_card, round_state["community_card"])
+
+
+
+  valid_actions = generate_possible_actions(round_state)
+  val_and_action = None
+  for action in valid_actions:
+    value = apply_action(round_state, action, bet_amount=10)
+    if value is None:
+       val_and_action = [value, action]
+    elif is_maximizing_player and value > val_and_action[0]:
+      val_and_action = [value, action]
+    elif not is_maximizing_player and value < val_and_action[0]:
+      val_and_action = [value, action]
+  return val_and_action
+  
+   
